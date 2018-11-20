78dc3202ce903e2be47bd1813a39901ac668e733
vaibhavid19 <vaibhavidoiphode@gmail.com>
2018-11-20 11:32:28 -0700

    Initial commit

---
+++ src/main/java/com/vaibhavi/characterservice/CharacterserviceApplication.java
@@ -0,0 +1,12 @@
+package com.vaibhavi.characterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vaibhavi/characterservice/CharacterserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vaibhavi.characterservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CharacterserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
